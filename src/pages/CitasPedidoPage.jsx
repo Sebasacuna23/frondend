e9b909e2f64b74c1155f1f356d9622670c4878f8
Hernando jr <nandojr26.hjjc@gmail.com>
2022-12-22 01:12:57 -0500

    identacion código CitasPedidoPage

--- src/pages/CitasPedidoPage.jsx
+++ src/pages/CitasPedidoPage.jsx
@@ -6,84 +6,72 @@ import { useParams } from "react-router-dom";
 import { findPedidoById, guardarPedido } from "../server/Server";
 
 function CitasPedidoPage() {
-    const {id} =useParams();
+    const { id } = useParams();
 
-    const [pedido,setPedido]=useState({});
+    const [pedido, setPedido] = useState({});
 
-    const [horaRadio, setHoraRadio]=useState("");
+    const [horaRadio, setHoraRadio] = useState("");
 
-    useEffect(()=>{
-
-        findPedidoById(id).then(data=>{
+    useEffect(() => {
+        findPedidoById(id).then(data => {
             setPedido(data);
 
         })
-    },[]);
+    }, []);
 
-    const handleSelect = (e) =>{
-        setHoraRadio(e.target.value);  
+    const handleSelect = (e) => {
+        setHoraRadio(e.target.value);
     };
 
-   async function handleSubmit(){
-    if (horaRadio!==""){
-        pedido.citas.push({hora:horaRadio,id_cliente:""});
-        guardarPedido(pedido);
-    }else{
-        alert("Seleccione un horario")
+    async function handleSubmit() {
+        if (horaRadio !== "") {
+            pedido.citas.push({ hora: horaRadio, id_cliente: "" });
+            guardarPedido(pedido);
+        } else {
+            alert("Seleccione un horario")
+        }
     }
-   }
 
-    const franjaHoraria=["8:00","8:30","9:00", "9:30","10:00", "10:30","11:00"];
+    const franjaHoraria = ["8:00", "8:30", "9:00", "9:30", "10:00", "10:30", "11:00"];
 
-
-
-
-    return(
+    return (
         <Container className="my-3">
-
             <Row>
                 <Col>
-                <p>Fecha Pedido: </p>
-                <h3>{pedido.fecha_despacho}</h3>
+                    <p>Fecha Pedido: </p>
+                    <h3>{pedido.fecha_despacho}</h3>
                 </Col>
                 <Col>
-                <p>nombre del Cliente :</p>
-                <h4>{pedido.nombrecliente}</h4>
+                    <p>nombre del Cliente :</p>
+                    <h4>{pedido.nombrecliente}</h4>
                 </Col>
             </Row>
             <Row className="my-3">
                 <Col xs lg="2">
-                Horarios disponibles:
+                    Horarios disponibles:
                 </Col>
                 <Col md="auto">
                     {
-                        franjaHoraria.map(hora =>(
+                        franjaHoraria.map(hora => (
                             <Form.Check className="my-3"
-                                 key={hora}
-                                 label={hora + " AM"}  
-                                 type="radio"
-                                 name="hora"
-                                 value={hora}
-                                 onChange={handleSelect}
-
-                />
-
+                                key={hora}
+                                label={hora + " AM"}
+                                type="radio"
+                                name="hora"
+                                value={hora}
+                                onChange={handleSelect}
+                            />
                         ))
                     }
-                
+
                 </Col>
-                {/* <Col xs lg="2">
-                <Form.Label># de Identificación del Cliente</Form.Label>
-                <Form.Control/>
-                </Col>*/}
             </Row>
             <Row>
                 <Col>
-                <Button variant="success" onClick={()=>handleSubmit()}>Registrar Pedido </Button>
+                    <Button variant="success" onClick={() => handleSubmit()}>Registrar Pedido </Button>
                 </Col>
             </Row>
-
         </Container>
     )
-    
-}export{CitasPedidoPage}
+
+} export { CitasPedidoPage }
